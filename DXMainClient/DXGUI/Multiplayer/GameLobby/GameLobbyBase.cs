b29bddb01d079ac866e90d590c950813715c3603
霜序零一 <942708116@qq.com>
2025-04-14 20:07:43 +0800

    update 250414
    Revert up_240920 and up_250205 to welcome the official null map bug fix

--- DXMainClient/DXGUI/Multiplayer/GameLobby/GameLobbyBase.cs
+++ DXMainClient/DXGUI/Multiplayer/GameLobby/GameLobbyBase.cs
@@ -507,25 +507,9 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
             randomMapWindow.Open();
         }
 
-        private void TbMapSearch_InputReceived(object sender, EventArgs e)
-        {
-            ListMaps();
+        private void TbMapSearch_InputReceived(object sender, EventArgs e) => ListMaps();
 
-            if (lbGameModeMapList.SelectedIndex == -1)
-                lbGameModeMapList.SelectedIndex = 0; // Select default GameModeMap
-            else
-                ChangeMap(GameModeMap);
-        }
-
-        private void MapScreenActived(object sender, EventArgs e)
-        {
-            ListMaps();
-
-            if (lbGameModeMapList.SelectedIndex == -1)
-                lbGameModeMapList.SelectedIndex = 0; // Select default GameModeMap
-            else
-                ChangeMap(GameModeMap);
-        }
+        private void MapScreenActived(object sender, EventArgs e) => ListMaps();
 
         private void Dropdown_SelectedIndexChanged(object sender, EventArgs e)
         {
